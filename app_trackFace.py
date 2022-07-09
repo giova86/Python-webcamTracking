ab1647873d959ec209fb88f1bc15afc65e606936
Giovanni Bocchi <giovanni.bocchi@kiwidatascience.com>
2022-07-09 12:17:19 +0200

    code updates

--- app_trackFace.py
+++ app_trackFace.py
@@ -15,30 +15,22 @@ while True:
         arr.append(index)
     cap.release()
     index += 1
+
+print('------------------------------------')
+print('Devices')
+print('------------------------------------')
 print(arr)
+print()
 
-start_x = 0
-start_y = 0
-stop_x = 1000
-stop_y = 600
+# width_cropped = width_out
+# height_cropped = height_out
+media_mobile = 100
 
-# mp_holistic = mp.solutions.holistic
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 mp_face_mesh = mp.solutions.face_mesh
-
 drawing_spec = mp_drawing.DrawingSpec(thickness=1, circle_radius=1)
 
-# logo = cv2.imread('logo.png', cv2.IMREAD_GRAYSCALE)
-# logo = cv2.flip(logo, 1)
-# scale_percent = 50 # percent of original size
-# width = int(logo.shape[1] * scale_percent / 100)
-# height = int(logo.shape[0] * scale_percent / 100)
-# dim = (width, height)
-# # resize image
-# logo = cv2.resize(logo, dim, interpolation = cv2.INTER_AREA)
-
-media_mobile = 100
 baricentro_x = []
 baricentro_y = []
 
@@ -47,12 +39,19 @@ vc = cv2.VideoCapture(1)
 if not vc.isOpened():
     raise RuntimeError('Could not open video source')
 
-pref_width = 1280
-pref_height = 720
-pref_fps = 30
-vc.set(cv2.CAP_PROP_FRAME_WIDTH, pref_width)
-vc.set(cv2.CAP_PROP_FRAME_HEIGHT, pref_height)
-vc.set(cv2.CAP_PROP_FPS, pref_fps)
+print('-- Camera Settings -----------------------------------')
+print(f'Height: {int(vc.get(cv2.CAP_PROP_FRAME_HEIGHT))}')
+print(f'Width: {int(vc.get(cv2.CAP_PROP_FRAME_WIDTH))}')
+print(f'FPS: {int(vc.get(cv2.CAP_PROP_FPS))}')
+print()
+
+# pref_width = 1280
+# pref_height = 720
+# pref_fps = 30
+
+# vc.set(cv2.CAP_PROP_FRAME_WIDTH, pref_width)
+# vc.set(cv2.CAP_PROP_FRAME_HEIGHT, pref_height)
+# vc.set(cv2.CAP_PROP_FPS, pref_fps)
 
 # Query final capture device values
 # (may be different from preferred settings)
@@ -64,6 +63,21 @@ fps = vc.get(cv2.CAP_PROP_FPS)
 #     min_detection_confidence=0.5,
 #     min_tracking_confidence=0.5) as holistic:
 
+zoom_scale = 1.5
+width_out = int(width/zoom_scale)
+height_out = int(height/zoom_scale)
+
+start_x = 0
+start_y = 0
+stop_x = width_out
+stop_y = height_out
+
+print('-- Output Settings -----------------------------------')
+print(f'Height: {height_out}')
+print(f'Width: {width_out}')
+print(f'Scale Factor: {zoom_scale}')
+print()
+
 with mp_face_mesh.FaceMesh(
     max_num_faces=1,
     refine_landmarks=True,
@@ -71,8 +85,11 @@ with mp_face_mesh.FaceMesh(
     min_tracking_confidence=0.5) as face_mesh:
 
     # with pyvirtualcam.Camera(width, height, fps, fmt=PixelFormat.BGR) as cam:
-    with pyvirtualcam.Camera(1000, 600, fps, fmt=PixelFormat.BGR) as cam:
+    with pyvirtualcam.Camera(width_out, height_out, fps, fmt=PixelFormat.BGR) as cam:
+        print()
         print('Virtual camera device: ' + cam.device)
+        print()
+
         while True:
             ret, image = vc.read()
 
@@ -98,7 +115,7 @@ with mp_face_mesh.FaceMesh(
 
                     x_face = (x_face_max + x_face_min)/2
                     y_face = (y_face_max + y_face_min)/2
-                    #print(x_face, y_face)
+
                     x_face=int(x_face*image.shape[1])
                     y_face=int(y_face*image.shape[0])
 
@@ -114,35 +131,34 @@ with mp_face_mesh.FaceMesh(
                         x_face = int(sum(baricentro_x)/len(baricentro_x))
                         y_face = int(sum(baricentro_y)/len(baricentro_y))
 
-                    if y_face - 300 < 0:
+                    if y_face - height_out/2 < 0:
                         start_y = 0
-                        stop_y = 600
-                    elif y_face + 300 > image.shape[0]:
-                        start_y =  image.shape[0] - 600
+                        stop_y = height_out
+                    elif y_face + height_out/2 > image.shape[0]:
+                        start_y =  image.shape[0] - height_out
                         stop_y = image.shape[0]
                     else:
-                        start_y = y_face-300
-                        stop_y = y_face+300
+                        start_y = y_face-height_out/2
+                        stop_y = y_face+height_out/2
 
-                    if x_face - 500 < 0:
+                    if x_face - width_out/2 < 0:
                         start_x = 0
-                        stop_x = 1000
-                    elif x_face + 500 > image.shape[1]:
-                        start_x = image.shape[1] - 1000
+                        stop_x = width_out
+                    elif x_face + width_out/2 > image.shape[1]:
+                        start_x = image.shape[1] - width_out
                         stop_x = image.shape[1]
                     else:
-                        start_x = x_face - 500
-                        stop_x = x_face + 500
-                    image_crop = image[start_y:stop_y, start_x:stop_x, :]
+                        start_x = x_face - width_out/2
+                        stop_x = x_face + width_out/2
+
+                    image_crop = image[int(start_y):int(stop_y), int(start_x):int(stop_x), :]
 
             else:
-                image_crop = image[start_y:stop_y, start_x:stop_x, :]
+                image_crop = image[int(start_y):int(stop_y), int(start_x):int(stop_x), :]
 
-            # draw_landmarks_custom(img, results2)
-            #frame = cv2.rotate(frame, cv2.ROTATE__CLOCKWISE)
-            # .. apply your filter ..
-            #image[(image_shape[0]-dim[1]-42):(image_shape[0]-42), (image_shape[1]-dim[0]-42):(image_shape[1]-42),:]=logo.reshape(dim[1],dim[0],1)
-            cv2.imshow('check video', image_crop)
-            #cv2.imshow('check video2', image_crop)          // Scommenta per testare
+            cv2.imshow('check video2', image_crop)          # Scommenta per testare
             cam.send(image_crop)
             cam.sleep_until_next_frame()
+            if cv2.waitKey(33) == ord('q'):
+                print("Quit system")
+                break
